4b1b73c7514da4a6be8560eb87d8047149135776
tanakancolle <tanakancolle@gmail.com>
2017-03-14 23:02:09 +0900

    Monobehaviour用シングルトンクラス追加

---
+++ Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    private static bool isQuit = false;
+
+    public static T Instance {
+        get{
+            if (isQuit) {
+                return null;
+            }
+
+            if (instance == null) {
+                instance = FindObjectOfType<T> () as T;
+                if (instance == null) {
+                    var obj = new GameObject (typeof(T).ToString() + "(Singleton)");
+                    instance = obj.AddComponent<T> ();
+
+                    DontDestroyOnLoad (obj);
+                }
+            }
+
+            return instance;
+        }   
+    }
+
+    protected SingletonMonoBehaviour()
+    {
+    }
+
+    void OnApplicationQuit()
+    {
+        isQuit = true;
+    }
+}
